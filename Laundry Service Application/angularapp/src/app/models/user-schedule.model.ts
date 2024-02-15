3e608c70ae7efc1acac97774530eba9a996d2c88
Jananipriya18 <jananijayachandran18@gmail.com>
2024-02-15 18:08:11 +0530

    Delete Laundry Service Application directory

--- Laundry Service Application/angularapp/src/app/models/user-schedule.model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserSchedule {
-  id: number;
-  fullName: string;
-  mobileNumber: string;
-  email: string;
-  address: string;
-  pickupDay: string;
-  pickupTimeSlot: string;
-}
